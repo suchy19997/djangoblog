31238e97081ef7aff8d854ee67bc81cddf20a332
suchy_97 <szycah2@gmail.com>
2023-01-27 21:05:35 +0100

    dodanie rekacja na klikniecie  na post

--- apka/views.py
+++ apka/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -8,7 +8,9 @@ def post_list(request):
     return render(request, 'apka/post_list.html', {'posts': posts})
 
 def VR(request):
-
     return render(request, 'apka/VR.html', )
 
+def post_detail(request,pk):
+    post=get_object_or_404(Post,pk= pk)
+    return render(request,'apka/post_detail.html',{'post':post})
 # Create your views here.
